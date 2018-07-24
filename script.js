03283fc143d0e43929e0d97270fd66ca17a8c638
syedaazhar <syedaazhar19@gmail.com>
2018-07-24 22:43:16 +0500

    form

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function validation()
+function validate() 
 {
     let user = document.querySelector('#username').value;
     let password = document.querySelector('#password').value;
@@ -7,88 +7,60 @@ function validation()
     let email = document.querySelector('#emailid').value;
 
     //username
-    if( user == "" ){
-        document.querySelector('#usernameid').innerHTML = " ** please enter a username ";
-        return false;
-    }
-    if(( user.length < 3) || (user.length > 20)){
-        document.querySelector('#usernameid').innerHTML = " **  username must be required 3 to 20 characters";
+    if ((user.length < 3) || (user.length > 20)) {
+        document.querySelector('#username-err').innerHTML = " **  username must be required 3 to 20 characters";
         return false;
     }
 
-    if(!isNaN(user)){
-        document.querySelector('#usernameid').innerHTML = " ** please enter character";
+    if (!isNaN(user)) {
+        document.querySelector('#username-err').innerHTML = " ** please enter character";
+
         return false;
     }
 
 
     //password
-
-    if( password == "" ){
-        document.querySelector('#passwords').innerHTML = " ** please enter a password ";
+    if ((password.length < 5) || (password.length > 20)) {
+        document.querySelector('#password-err').innerHTML = " ** please fill the password between 5 and 20";
         return false;
     }
 
-    if((password.length < 5) || (password.length > 20)){
-        document.querySelector('#passwords').innerHTML = " ** please fill the password between 5 and 20";
+    //confrmpassword
+
+    if (password != confrmpassword) {
+        document.querySelector('#passwordconfrm-err').innerHTML = " **  password are not matching";
         return false;
     }
 
-     //confrmpassword
-
-    if( password!=confrmpassword){
-        document.querySelector('#confrmpasswrod').innerHTML = " **  password are not matching";
-        return false;
-    }
-
-
-
-    if( confrmpassword == "" ){
-        document.querySelector('#confrmpasswrod').innerHTML = " ** please fill the confrm paasword ";
-        return false;
-    }
 
     //mobileNumb
 
-    if( mobileno == "" ){
-        document.querySelector('#mobilenumber').innerHTML = " ** please fill the mobile number ";
-        return false;
-    }
 
-    if(mobileno.length!=11){
-        document.querySelector('#mobilenumber').innerHTML = " ** Mobile number should be 10 digits ";
+    if (mobileno.length != 11) {
+        document.querySelector('#numbermobile-err').innerHTML = " ** Mobile number should be 10 digits ";
         return false;
     }
-    if(isNaN(mobileno)){
-        document.querySelector('#mobilenumber').innerHTML = " ** Mobile number should conatins only digits ";
+    if (isNaN(mobileno)) {
+        document.querySelector('#numbermobile-err').innerHTML = " ** Mobile number should conatins only digits ";
         return false;
     }
 
     //email
 
-    if( email == "" ){
-        document.querySelector('#emailids').innerHTML = " ** please fill the email id ";
+
+    if (email.indexOf('@') <= 0) {
+        document.querySelector('#emailid-err').innerHTML = " ** please fill the email id i proper format @ ";
         return false;
     }
 
 
-    if(email.indexOf('@') <= 0){
-        document.querySelector('#emailids').innerHTML = " ** please fill the email id i proper format @ ";
+
+    if ((email.charAt(email.length - 4) != '.') && (email.charAt(email.length - 3) != '.')) {
+
+        document.querySelector('#emailid-err').innerHTML = " ** please fill the email id in proper format . ";
         return false;
     }
 
-        
-
-    if((email.charAt(email.length-4)!='.') && (email.charAt(email.length-3)!='.')){
-
-        document.querySelector('#emailids').innerHTML = " ** please fill the email id in proper format . ";
-        return false;
-    } 
-
-   
-
-}   
 
 
-
-
+}
